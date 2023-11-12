b388cfe316a3782e5ccbfcb5e460d495cadc5981
9weights <68071656+9weights@users.noreply.github.com>
2023-11-12 21:21:11 +0000

    fix for git push macro

--- lua/mfdoom/gitMacros.lua
+++ lua/mfdoom/gitMacros.lua
@@ -1,10 +1,10 @@
-vim.keymap.set('n',"<leader>pa", function()
+vim.keymap.set('n',"<leader>p1", function()
     gitUser = "9weights"
     gitKey = "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"
     fireExit = vim.api.nvim_replace_termcodes("<C-c>", false, false, true)
     carRet = vim.api.nvim_replace_termcodes("<CR>", false, false, true)
     vim.api.nvim_feedkeys("i" .. gitUser .. carRet, 'n', false)
-    vim.api.nvim_feedkeys(gitKey .. carRet, 'n', false)
+    --vim.api.nvim_feedkeys(gitKey .. carRet, 'n', false)
     --vim.cmd("call feedkeys('i" .. gitUser .. carRet"')")
     --vim.cmd("call feedkeys('i" .. gitKey .. carRet"')")
     --vim.cmd.chansend(window_channel, "ls\n" )
@@ -12,4 +12,10 @@ vim.keymap.set('n',"<leader>pa", function()
     --vim.cmd(call feedkeys(gitUser .. '<cr>'))
     --vim.cmd(call feedkeys(gitKey .. "<cr>"))
 end)
-
+vim.keymap.set('n',"<leader>p2", function()
+    gitUser = "9weights"
+    gitKey = "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"
+    fireExit = vim.api.nvim_replace_termcodes("<C-c>", false, false, true)
+    carRet = vim.api.nvim_replace_termcodes("<CR>", false, false, true)
+    vim.api.nvim_feedkeys("i" .. gitKey .. carRet, 'n', false)
+end)
