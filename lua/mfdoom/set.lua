46e764e28f5efad440b588b5e42f65eb51bbabb7
9weights <68071656+9weights@users.noreply.github.com>
2024-01-06 21:46:52 +0000

    nice folds

--- lua/mfdoom/set.lua
+++ lua/mfdoom/set.lua
@@ -25,3 +25,12 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.updatetime = 750
 vim.opt.fillchars = { eob = ' ' }
+
+-- fold settings
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
+vim.wo.foldtext =
+    [[substitute(getline(v:foldstart),'\\t',repeat('\ ',&tabstop),'g').'...'.trim(getline(v:foldend)) ]]
+vim.wo.fillchars = "fold:\\"
+vim.wo.foldnestmax = 3
+vim.wo.foldminlines = 1
